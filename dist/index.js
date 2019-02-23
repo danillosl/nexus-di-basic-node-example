d726b3aec21c945c98f4a1bf28681c7c9cdbb07d
danillosl <danillosl@gmail.com>
2019-02-22 21:31:37 -0300

    removing build and start scripts and removing dist folder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _require = require("nexus-di"),
-    container = _require.container;
-
-var Battle = require("./Battle");
-
-var battle = container.getInstance(Battle);
-console.log(battle.fight());
