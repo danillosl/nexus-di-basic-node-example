168799928f9675cf8412396167515761c06957e1
danillosl <danillosl@gmail.com>
2019-02-25 00:45:22 -0300

    update to 0.4.0

--- src/containerConfig.js
+++ src/containerConfig.js
@@ -3,8 +3,8 @@ const Katana = require("../src/weapons/Katana");
 const Shuriken = require("../src/weapons/Shuriken");
 
 container
-  .register({ target: Katana, scope: SCOPES.SINGLETON })
-  .register({ target: Shuriken, scope: SCOPES.PROTOTYPE })
+  .register(Katana, { scope: SCOPES.SINGLETON })
+  .register(Shuriken, { scope: SCOPES.PROTOTYPE })
   .registerObject({
     key: "SHIELD",
     provider: container => ({ name: "shield", armor: 15 }),
